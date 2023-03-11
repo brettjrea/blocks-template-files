40ff8851eeffd08fd1082e6dca23c16a8f0fac7b
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-03-11 13:49:40 -0500

    Update index.tsx

--- blocks/example-file-block/index.tsx
+++ blocks/example-file-block/index.tsx
@@ -49,15 +49,6 @@ export default function ExampleFileBlock(props: ExampleFileBlockProps) {
           File: {context.path} {language}
         </Box>
         <Box p={4}>
-          <p>Metadata example: this button has been clicked:</p>
-          <Button
-            onClick={() =>
-              onUpdateMetadata({ number: (metadata.number || 0) + 1 })
-            }
-          >
-            {metadata.number || 0} times
-          </Button>
-          <pre className="mt-3 p-3">{content}</pre>
           {language === "plantuml" && (
             <ReactPlantUML src={content} alt="PlantUML diagram" />
           )}
